c5ec5821ac9a5e6215d744faf019e31184537e6a
Nicole Loew <nicoleal@users.noreply.github.com>
2016-05-29 10:16:55 -0400

    27May16_v1
    Added additional builder methods.

--- src/Tree.java
+++ src/Tree.java
@@ -5,7 +5,7 @@
  * 			An implementation of the most general Tree building class possible,
  * 			trying to maintain both code flexibility and consistency, allowing for
  * 			multiple types of trees to be built, but only one kind of tree to be
- * 			built at a time, et hoc genus omne. 
+ * 			built at a time, et hoc genus omne.
  * 
  * @author Nicole Loew
  * @version CS5999 Graduate Thesis Spring 2017; 27 May 2016
